1feb91e0959df933a94621ad67090b54faf7083a
Daniel Gholiof <daniel.gholiof@swace.se>
2025-11-20 08:55:09 +0100

    Configure project with credentials in app config, set up github actions for deploying, update .gitignore
    The release-app action triggers when a change is made to app-version.js
    (usually bumping the version numbers)

---
+++ app.config.ts
@@ -0,0 +1,53 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+import { androidVersionCode, iosBuildNumber, version } from "./app-version";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "DoPlan",
+  slug: "doplan",
+  version,
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "doplan",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.doplan.doplan",
+    buildNumber: iosBuildNumber,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    versionCode: androidVersionCode,
+    package: "com.doplan.doplan",
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    eas: {
+      projectId: "7f860522-b94f-4dbb-b403-9cf63e7b3bce",
+    },
+  },
+});
